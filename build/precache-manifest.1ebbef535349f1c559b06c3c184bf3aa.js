0096f0077af60a8a790b6e4198ae6614c8a7c000
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2019-09-04 09:55:30 -0700

    add build for gh-pages

---
+++ build/precache-manifest.1ebbef535349f1c559b06c3c184bf3aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105fab1919ef9c2d26971781fd6e7ccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbdda25a9d255c3a8634",
+    "url": "/static/css/main.5ec08dbb.chunk.css"
+  },
+  {
+    "revision": "c48da0fed2ae99bf44e9",
+    "url": "/static/js/2.7364ad56.chunk.js"
+  },
+  {
+    "revision": "dbdda25a9d255c3a8634",
+    "url": "/static/js/main.2f6e4e8e.chunk.js"
+  },
+  {
+    "revision": "590fae1f0248acb07b32",
+    "url": "/static/js/runtime~main.1547a8a5.js"
+  }
+]);
